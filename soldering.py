9c83e2307359bd097e30c22811e6db2593de824e
Ilya Gavrilin <89260891+Ilyagavrilin@users.noreply.github.com>
2022-04-13 15:06:45 +0300

    Update soldering.py

--- soldering.py
+++ soldering.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import random
 
-values = [randint(-100, 100) for i in range(20)]
+values = [random.randint(-100, 100) for i in range(20)]
 
-with open("data.txt", 'r') :
-  write('\n'.join([str(value) for value in values]))
+with open("data.txt", 'r') as f:
+  f.write('\n'.join([str(value) for value in values]))
   
 plt.plot(values)
 plt.show()
